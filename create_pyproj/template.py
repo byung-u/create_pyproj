45198aa445ca30f4fec7fecb89d78a69b961df26
Byungwoo Jeon <iam.byungwoo@gmail.com>
2017-03-12 22:09:33 +0900

    ADD create new directory and files

--- create_pyproj/template.py
+++ create_pyproj/template.py
@@ -2,33 +2,69 @@
 
 from create_pyproj.options import Options
 
-import argparse
 import os
+import shutil
 import sys
 from typing import List, Tuple
 
 
 def get_path_and_proj(options: Options) -> Tuple[str, str]:
     if options.path is None and options.proj is None:
-        path = './new_project'
-        proj = 'new_project'
+        options.path = './new_project'
+        options.proj = 'new_project'
     elif options.path is None and options.proj is not None:
-        path = './new_project'
-        proj = options.proj
-    elif options.path is None and options.proj is None:
-        path = options.path
-        proj = 'new_project'
-    else:
-        path = options.path
-        proj = options.proj
+        options.path = './new_project'
+    elif options.path is not None and options.proj is None:
+        options.proj = 'new_project'
 
-    return path, proj
+    return options
 
 def create_python_template(options: Options) -> bool:
 
-    path, proj = get_path_and_proj(options)
+    options = get_path_and_proj(options)
     #1. create directory
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if os.path.exists(options.path):
+        # remove last template
+        print('remove dir:', options.path)
+        shutil.rmtree(options.path)
 
-    print(path, proj)
+    os.makedirs(options.path)
+    os.chdir(options.path)
+     
+    create_file('LICENSE', options)
+    create_file('README.md', options)
+    create_file('TODO.md', options)
+
+
+def create_file(file_name, options):
+    fw = open(file_name, 'w')
+    if (file_name == 'LICENSE'):
+        fw.write('''MIT License
+
+Copyright (c) %d %s
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.''' % (options.year, options.auth))
+    elif (file_name == 'README.md'):
+        fw.write('''%s
+--------------------
+%s''' % (options.proj, options.proj))
+    elif (file_name == 'TODO.md'):
+        fw.write('-')
+
+    fw.close()
